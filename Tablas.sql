5bf2b27978030ab3f98c231a5ae4648db65d664b
Ramses Cordoba <166573615+GodotCoffee28@users.noreply.github.com>
2026-06-30 06:25:52 -0400

    Creacion de todas las tablas pertinentes

---
+++ Tablas.sql
@@ -0,0 +1,102 @@
+-- Creaciones de tablas -- 
+
+DROP TABLE IF EXISTS Persona CASCADE;
+DROP  TABLE  IF EXISTS Usuario CASCADE;
+DROP TYPE IF EXISTS tipo_plan;
+DROP TABLE IF EXISTS Artista CASCADE;
+DROP TABLE IF EXISTS Playlist CASCADE;
+DROP TABLE IF EXISTS Album CASCADE;
+DROP TABLE IF EXISTS Cancion CASCADE;
+DROP TABLE IF EXISTS Archivo_Audio CASCADE;
+DROP TABLE IF EXISTS Escucha CASCADE;
+DROP TABLE IF EXISTS Contiene CASCADE;
+
+CREATE TABLE Persona(
+	id_persona smallint NOT NULL,
+	email varchar(60),
+	contrasenia  varchar(30),
+	fecha_registro date,
+	fecha_nacimiento date,
+	nombre varchar(20),
+
+	PRIMARY KEY(id_persona)
+);
+
+CREATE TYPE tipo_plan AS ENUM('Premiun','Gratis');
+CREATE TABLE Usuario(
+	id_usuario smallint NOT NULL,
+	tipo tipo_plan,
+
+	PRIMARY KEY(id_usuario),
+	FOREIGN KEY(id_usuario) REFERENCES  Persona(id_persona)
+);
+
+CREATE TABLE Artista(
+	id_artista smallint NOT NULL,
+	biografia text,
+
+	PRIMARY KEY(id_artista),
+	FOREIGN KEY(id_artista) REFERENCES Persona(id_persona)
+);
+
+CREATE TABLE Playlist(
+	id_playlist smallint NOT NULL,
+	nombre varchar(20),
+	descripcion text,
+
+	PRIMARY KEY(id_playlist)
+);
+
+
+CREATE TABLE Album(
+	id_album smallint NOT NULL,
+	id_artista_album smallint NOT NULL,
+	titulo varchar(20),
+	fecha_lanzamiento date,
+
+	PRIMARY KEY(id_album),
+	FOREIGN KEY(id_artista_album) REFERENCES Artista(id_artista)
+);
+
+CREATE TABLE Cancion(
+	id_cancion smallint NOT NULL,
+	id_album_cancion smallint NOT NULL,
+	titulo varchar(20),
+	genero varchar(20),
+	duracion_seg smallint,
+
+	PRIMARY KEY(id_cancion),
+	FOREIGN KEY(id_album_cancion) REFERENCES Album(id_album)
+);
+
+CREATE TABLE Archivo_Audio(
+	id_audio smallint NOT NULL,
+	id_cancion_audio smallint NOT NULL,
+	URL text,
+	tamanio decimal,
+	formato varchar(5),
+	ubicacion text,
+	calidad text,
+
+	PRIMARY KEY(id_audio,id_cancion_audio),
+	FOREIGN KEY(id_cancion_audio) REFERENCES Cancion(id_cancion)
+);
+
+CREATE TABLE Escucha(
+	id_usuario_escucha smallint NOT NULL,
+	id_playlist_escucha smallint NOT NULL,
+
+	PRIMARY KEY(id_usuario_escucha, id_playlist_escucha),
+	FOREIGN KEY(id_usuario_escucha) REFERENCES Usuario(id_usuario) ON DELETE CASCADE,
+	FOREIGN KEY(id_playlist_escucha) REFERENCES Playlist(id_playlist) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Contiene(
+	id_playlist_contiene smallint NOT NULL,
+	id_cancion_contiene smallint NOT NULL, 
+
+	PRIMARY KEY(id_playlist_contiene, id_cancion_contiene),
+	FOREIGN KEY(id_playlist_contiene) REFERENCES Playlist(id_playlist) ON DELETE CASCADE,
+	FOREIGN KEY(id_cancion_contiene) REFERENCES Cancion(id_cancion) ON DELETE CASCADE
+);
